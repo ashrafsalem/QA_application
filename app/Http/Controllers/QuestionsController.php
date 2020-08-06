ffe847613d4b45d14939a800164ac9bd26ed67ea
ashrafsalem <eng.ashrafsalem@gmail.com>
2020-08-06 10:47:06 +0200

    adding auther and create date fro question and add barryvdh/laravel-debugbar --dev

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -10,7 +10,15 @@ class QuestionsController extends Controller
 
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
+        // solving lazy loading problem , with eager Loading , solving alleviates n+1 problem
+        // here if i not set the relation with query will load all the users related to questions
+        // it will load first to rows . question count and questions data , the rest will be the users that will be used
+//        \DB::enableQueryLog();
+//        $questions = Question::with('user')->latest()->paginate(10);
+//        view('question.index', compact('questions'))->render();
+//        dd(\DB::getQueryLog());
+
+        $questions = Question::with('user')->latest()->paginate(5);
         return view('question.index', compact('questions'));
     }
 
