f5e9bb667dbaf5d86cfec95e93142d845674364d
ashrafsalem <eng.ashrafsalem@gmail.com>
2020-08-17 12:48:49 +0200

    adding answers to question

--- app/User.php
+++ app/User.php
@@ -51,4 +51,13 @@ class User extends Authenticatable
     {
         return $this->hasMany();
     }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 32;
+
+       return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+
+    }
 }
